66e3ee34d91b3de8ae1629ce34e61825c70ac679
Sanelisiwe Myburg <sane.myburg@gmail.com>
2022-11-10 11:57:03 +0200

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import './style.css';
+
 const arrayOfObjects = [
-    {
-        index: 1,
-        description: "wash the dishes",
-        completed: false,
-    },
-    {
-        index: 1,
-        description: "complete To Do list project",
-        completed: false,
-    }
+  {
+    index: 1,
+    description: 'wash the dishes',
+    completed: false,
+  },
+  {
+    index: 1,
+    description: 'complete To Do list project',
+    completed: false,
+  },
 ];
 
 const todoTask = document.querySelector('.to-do-task');
-    arrayOfObjects.forEach((element) => {
-        const div = document.createElement('div');
-        div.classList.add("todo");
-        div.innerHTML += `
+arrayOfObjects.forEach((element) => {
+  const div = document.createElement('div');
+  div.classList.add('todo');
+  div.innerHTML += `
 
           <i class="uil uil-square-full"></i>
           <p class="">${element.description}</p>
           <i class="uil uil-ellipsis-v"></i>
     `;
-    todoTask.append(div);
-
-    });
-    
+  todoTask.append(div);
+});
