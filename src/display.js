68b0633a0b83b56f551c80138e815dee2ee12b26
Sanelisiwe Myburg <sane.myburg@gmail.com>
2022-11-12 17:48:18 +0200

    Fix linter errors

--- src/display.js
+++ src/display.js
@@ -1,20 +1,102 @@
-const taskInput = document.querySelector('#add-list');
-const form = document.querySelector('.list-form');
-const addBtn = document.querySelector('#add-list-btn');
-const todoTask = document.querySelector('.to-do-task');
-let arrayOfObjects = JSON.parse(localStorage.getItem('todo-list')) || [];
+import { arrayOfObjects, showToDo } from './index.js';
 
-
-
-
-  const editTask = (taskId, taskName)  => {
-    let editId;
-    let isEditedTask = false;
-        editId = taskId;
-        isEditedTask = true;
-        taskInput.value = taskName;
-    
-        console.log(taskId, taskName);
+export const createTask = (todo, id) => {
+    const mainDiv = document.createElement('div');
+    mainDiv.classList.add('todo');
+  
+    const innerDiv1 = document.createElement('div');
+    innerDiv1.classList.add('todo-div');
+    innerDiv1.dataset.id = id;
+  
+    const editableInput = document.createElement('INPUT');
+    editableInput.value = todo.description;
+    editableInput.setAttribute('readonly', true);
+    if (todo.complete) {
+      editableInput.style.textDecoration = 'line-through';
+    } else {
+      editableInput.style.textDecoration = 'none';
     }
+  
+    editableInput.addEventListener('click', () => {
+      editableInput.removeAttribute('readonly');
+    });
+  
+    editableInput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        const newValue = editableInput.value;
+        editableInput.setAttribute('readonly', true);
+        arrayOfObjects[id].description = newValue;
+        localStorage.setItem('todo-list', JSON.stringify(arrayOfObjects));
+        // showToDo()
+      }
+    });
+    const checkboxElement = document.createElement('INPUT');
+    checkboxElement.setAttribute('type', 'checkbox');
+    checkboxElement.dataset.id = id;
+    checkboxElement.checked = todo.complete;
+  
+    checkboxElement.addEventListener('change', () => {
+      arrayOfObjects[id].complete = checkboxElement.checked;
+      localStorage.setItem('todo-list', JSON.stringify(arrayOfObjects));
+      if (checkboxElement.checked) {
+        editableInput.style.textDecoration = 'line-through';
+      } else {
+        editableInput.style.textDecoration = 'none';
+      }
+    });
+  
+    innerDiv1.appendChild(checkboxElement);
+    innerDiv1.appendChild(editableInput);
+  
+    const innerDiv2 = document.createElement('div');
+    innerDiv2.classList.add('todo-i');
+  
+    const trashIcon = document.createElement('i');
+    trashIcon.classList.add('fa', 'fa-trash');
+  
+    trashIcon.addEventListener('click', () => {
+      arrayOfObjects.splice(id, 1);
+      localStorage.setItem('todo-list', JSON.stringify(arrayOfObjects));
+      showToDo();
+    });
+  
+    innerDiv2.appendChild(trashIcon);
+  
+    mainDiv.appendChild(innerDiv1);
+    mainDiv.appendChild(innerDiv2);
+  
+    return mainDiv;
+  };
 
-    export default editTask
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const todoTask = document.querySelector('.to-do-task');
+//   const showToDo = () => {
+//     todoTask.innerHTML = '';
+//     if (arrayOfObjects) {
+//       arrayOfObjects.forEach((todo, id) => {
+//         const newTask = createTask(todo, id);
+//         todoTask.appendChild(newTask);
+//       });
+//     }
+//   };
+
+//   export {showToDo}
+  
--- src/index.js
+++ src/index.js
@@ -1,80 +1,40 @@
-// import editTask from './display';
 import './style.css';
+import { createTask } from './display';
 
 const taskInput = document.querySelector('#add-list');
-const form = document.querySelector('.list-form');
-const addBtn = document.querySelector('#add-list-btn');
 const todoTask = document.querySelector('.to-do-task');
-let arrayOfObjects = JSON.parse(localStorage.getItem('todo-list')) || [];
+export let arrayOfObjects = JSON.parse(localStorage.getItem('todo-list')) || [];
 
-// let editId;
-// let isEditedTask = false;
+const clearAllButton = document.getElementById('clear-all-button');
 
- const showToDo = () => {
+
+export const showToDo = () => {
   todoTask.innerHTML = '';
-    if (arrayOfObjects) {
-       arrayOfObjects.forEach((todo, id) => {
-        let isCompleted = todo.complete === true ? 'checked' : '';
-          todoTask.innerHTML += `
+  if (arrayOfObjects) {
+    arrayOfObjects.forEach((todo, id) => {
+      const newTask = createTask(todo, id);
+      todoTask.appendChild(newTask);
+    });
+  }
+};
 
-          <div class="todo">
-            <div class="todo-div" id="${id}">
-              <input onclick="updateStatus(this)" type="checkbox" id="${id}">
-              <p class="${isCompleted}">${todo.description}</p>
-            </div> 
-            <div class="todo-i">
-             <i onclick="deleteTask(${id})" class="fa fa-trash"></i>
-             <i onclick="editTask(${id},  '${todo.description}')" class="fa fa-ellipsis-v"></i>
-            </div>
-          </div> `;
-    
-        });
-    }    
-}
 
 showToDo();
-editTask();
 
+// Create new Todo
+taskInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    const userTask = taskInput.value.trim();
 
-const deleteTask = (deletedId) => {
-    arrayOfObjects.splice(deletedId, 1);
-    localStorage.setItem('todo-list', JSON.stringify(arrayOfObjects));
-    showToDo();
-    console.log(deletedId);
-
-}
-
-const updateStatus = (selectedTask) => {
-    let taskName = selectedTask.parentElement.lastElementChild;
-    if (selectedTask.checked) {
-        taskName.classList.add('checked');
-        arrayOfObjects[selectedTask.id].complete = true;
-    } else {
-        taskName.classList.remove('checked');
-        arrayOfObjects[selectedTask.id].complete = false;
+    if (!arrayOfObjects) { // if list is empty, pass empty array
+      arrayOfObjects = [];
     }
+    const taskInfo = { description: userTask, complete: false };
+    arrayOfObjects.push(taskInfo);
+
+    taskInput.value = '';
     localStorage.setItem('todo-list', JSON.stringify(arrayOfObjects));
-    console.log(selectedTask);
-
-}
-
-taskInput.addEventListener('keypress', e => {
-    console.log('keypress')
-    if (e.key === 'Enter') {
-        let userTask = taskInput.value.trim();
-        console.log(userTask);
-        if (isEditedTask) {
-            if(!arrayOfObjects) { // if list is empty, pass empty array
-                arrayOfObjects = [];
-        }
-        let taskInfo = {description: userTask, complete: false};
-        arrayOfObjects.push(taskInfo);  
-        console.log(taskInfo);  
-        } else {
-            isEditedTask = false;
-            arrayOfObjects[editId].description = userTask;
-        }
-        taskInput.value = '';
-        localStorage.setItem('todo-list', JSON.stringify(arrayOfObjects));
-    }    
+  }
 });
+
+
